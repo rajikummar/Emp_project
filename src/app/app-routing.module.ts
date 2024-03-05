69846f99b6ccb1773702e7909f8ace009d20dd2a
navikumar <navid0399@gmail.com>
2024-03-05 23:45:49 +0530

    route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JobdemplyoeeComponent } from './jobdemplyoee/jobdemplyoee.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  pathMatch: 'full',
+  redirectTo: 'employee'
+},
+{
+path:'employee',component:JobdemplyoeeComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
